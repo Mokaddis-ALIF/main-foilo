1d99e37e7336da398329e63cd07709983bda5a74
Alif-07 <mokaddis.alif@gmail.com>
2022-01-05 23:03:11 +0600

    responsive

--- src/components/body/about/about.js
+++ src/components/body/about/about.js
@@ -7,7 +7,7 @@ const About = () => {
 	return (
 		<div className="about">
 			<div className="about-top">
-				<div className="about-info">
+				{/* <div className="about-info">
 					Hi, I'm&nbsp;<span className="my-name">ALIF</span>
 					<br />
 					<div className="i-title">
@@ -31,7 +31,23 @@ const About = () => {
 							Download Resume
 						</a>
 					</div>
+				</div> */}
+				<div className="i-left">
+					<div className="i-left-wrapper">
+						<h2 className="i-intro">Hello, My name is</h2>
+						<h1 className="i-name">ALIF</h1>
+						<div className="i-title">
+							<div className="i-title-wrapper">
+								<div className="i-title-item">Front-end Web Developer</div>
+								<div className="i-title-item">JavaScript Developer</div>
+								<div className="i-title-item">MERN Stack Developer</div>
+								<div className="i-title-item">Computer Science Engineer</div>
+								<div className="i-title-item">Photographer</div>
+							</div>
+						</div>
+					</div>
 				</div>
+
 				<div className="about-photo">
 					<img src={img} alt="" className="picture" />
 				</div>
