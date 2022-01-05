97de21456fba5cf52bc266b21f4173ef24a9bd1c
Alif-07 <mokaddis.alif@gmail.com>
2022-01-05 21:51:02 +0600

    final

--- src/components/body/about/about.js
+++ src/components/body/about/about.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './about.css';
 import img from '../../../png/Source code-bro.png';
-import SocialContact from '../../commons/social-contact/SocialContact';
 import { UilDownloadAlt } from '@iconscout/react-unicons';
 
 const About = () => {
@@ -23,20 +22,18 @@ const About = () => {
 						sizes,specializing in creating stylish,unique & all kinds of modern
 						website
 					</p>
+					<div className="download">
+						<a
+							download
+							href="https://drive.google.com/file/d/1onKnF96cdEf2oLInVDnLRXLEskIGw8YV/view?usp=sharing"
+						>
+							<UilDownloadAlt />
+							Download Resume
+						</a>
+					</div>
 				</div>
 				<div className="about-photo">
 					<img src={img} alt="" className="picture" />
-				</div>
-			</div>
-			<div className="contact-resume">
-				<div className="download">
-					<a
-						download
-						href="https://drive.google.com/file/d/1onKnF96cdEf2oLInVDnLRXLEskIGw8YV/view?usp=sharing"
-					>
-						<UilDownloadAlt />
-						Download Resume
-					</a>
 				</div>
 			</div>
 		</div>
--- src/components/header/web/Web.js
+++ src/components/header/web/Web.js
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from '../../../Context';
 import './Web.css';
 
 const Web = () => {
+	const theme = useContext(ThemeContext);
+	const darkMode = theme.state.darkMode;
+
 	return (
 		<div className="web">
 			<div className="web-options">
-				<a href="#projects">
+				<a
+					href="#projects"
+					style={{
+						color: darkMode && 'white',
+					}}
+				>
 					<i className="fi fi-rr-archive option-icon"></i>Projects
 				</a>
 			</div>
 			<div className="web-options">
-				<a href="#skills">
+				<a
+					href="#skills"
+					style={{
+						color: darkMode && 'white',
+					}}
+				>
 					<i className="fi fi-rr-laptop option-icon"></i>Skills
 				</a>
 			</div>
 			<div className="web-options">
-				<a href="#work">
+				<a
+					href="#work"
+					style={{
+						color: darkMode && 'white',
+					}}
+				>
 					<i className="fi fi-rr-briefcase option-icon"></i>Work
 				</a>
 			</div>
 			<div className="web-options">
-				<a href="#contact">
+				<a
+					href="#contact"
+					style={{
+						color: darkMode && 'white',
+					}}
+				>
 					<i className="fi fi-rr-mode-portrait option-icon"></i>Contact
 				</a>
 			</div>
