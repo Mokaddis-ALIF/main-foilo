d3d34f79b3d7ba7d5a1f47e24b0ced8caca3b03a
Alif-07 <mokaddis.alif@gmail.com>
2022-01-05 20:24:42 +0600

    icon size

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useContext } from 'react';
 import './App.css';
 import Home from './components/home/Home';
+import { ThemeContext } from './Context';
 
 function App() {
+	const theme = useContext(ThemeContext);
+	const darkMode = theme.state.darkMode;
+
 	return (
-		<>
+		<div
+			style={{
+				backgroundColor: darkMode ? '#222' : 'white',
+				color: darkMode && 'white',
+			}}
+		>
 			<Home />
-		</>
+		</div>
 	);
 }
 
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Home.css';
 import Header from '../header/Header';
 import Body from '../body/Body';
 import Footer from '../footer/Footer';
+import Toggle from '../toogle/Toggle';
+import { ThemeContext } from '../../Context';
 
 const Home = () => {
+	const theme = useContext(ThemeContext);
+	const darkMode = theme.state.darkMode;
 	return (
-		<div className="home">
+		<div
+			className="home"
+			style={{
+				backgroundColor: darkMode ? '#222' : 'white',
+				color: darkMode && 'white',
+			}}
+		>
+			<Toggle />
 			<Header />
 			<Body />
 			<Footer />
